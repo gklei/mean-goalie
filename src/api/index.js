b47f5c438bc54ac7e0291cf37466b977b7ec6d60
gklei <gregory@incipia.co>
2017-08-19 15:03:12 -0400

    Set up routing.

---
+++ src/api/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+var tasks = require('../../mock/tasks.json');
+
+router.get('/tasks', function (req, res) {
+  res.json({
+    tasks : tasks
+  });
+});
+
+module.exports = router;
