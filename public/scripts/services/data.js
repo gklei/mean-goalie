9d4c624d31969c3bbf5940dd4fe3fa0c886b58cb
gklei <gregory@incipia.co>
2017-08-20 13:50:40 -0500

    Fixed a bug.

--- public/scripts/services/data.js
+++ public/scripts/services/data.js
@@ -3,7 +3,7 @@
 angular.module('todoListApp')
 .service('dataService', function($http) {
   this.getTodos = function(cb) {
-    $http.get('/mock/tasks.json').then(cb);
+    $http.get('../../mock/tasks.json').then(cb);
   };
   
   this.deleteTodo = function(todo) {
--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var router = express.Router();
-var tasks = require('../../mock/tasks.json');
+var tasks = require('../../public/mock/tasks.json');
 
 router.get('/tasks', function (req, res) {
   res.json({
